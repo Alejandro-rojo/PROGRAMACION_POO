a19fb92bebddc0c9c50f769ba804a77401bb057b
unknown <juantamayomanzano@gmail.com>
2025-10-27 17:30:50 -0500

    poo

---
+++ Ejercicios_poo.py
@@ -0,0 +1,29 @@
+# #ejercicio 1
+
+# class persona():
+#     def __init__(self,nombre,edad):
+#         self.nombre = nombre
+#         self.edad = edad
+#     def saludo(self):
+#        print(f"hola {self.nombre} cuantos años tienes? {self.edad}?")
+# class amigo(persona):
+#     def saludo(self):
+#         print(f"hola {self.nombre} como va tu mama?, cuantos años tienes? {self.edad}?")
+# usuario1=amigo(input("ingresa un nombre: "),int(input("ingresa tu edad: ")))
+# usuario2=persona(input("ingresa un nombre: "),int(input("ingresa tu edad: ")))
+# usuario1.saludo()
+# usuario2.saludo()
+
+#ejercicio 2
+# class estudiante():
+#     def __init__(self,nombre,nota_matematicas,nota_ingles,nota_biologia,):
+#         self.nombre = nombre
+#         self.nota_matematicas = nota_matematicas
+#         self.nota_ingles = nota_ingles
+#         self.nota_biologia = nota_biologia
+#     def promedio(self):
+#         promedio1=(self.nota_matematicas+self.nota_ingles+self.nota_biologia)/3
+#         print(f"hola {self.nombre} tu promedio es de {promedio1}")
+
+# persona=estudiante(input("ingresa tu nombre: "),float(input("ingresa tu nota en matematicas: ")),float(input("ingresa tu nota en ingles: ")),float(input("ingresa tu nota en biologia: ")))
+# persona.promedio()
